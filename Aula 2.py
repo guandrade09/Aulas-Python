6528059acc1ba1a8292e26517ebf5e853f042bb8
guandrade09 <gustavoandradsousa00@gmail.com>
2024-03-06 19:56:46 -0300

    Primeira aula em Python!

---
+++ Aula 2.py
@@ -0,0 +1,25 @@
+#Aula 2 - Computational Thinking for Engineering
+
+print("Hello World!")
+
+#Exercício 1
+
+print("Gustavo Andrade de Sousa")
+
+#Exercício 2
+
+a = 5
+b = 3
+
+print(2*a*3*b)
+
+#Exercício 3
+
+c = int(input("Digite o primeiro número: "))
+d = int(input("Digite o segundo número: "))
+e = int(input("Digite o terceiro número: "))
+
+soma = c + d + e
+
+print("Resultado:")
+print(soma)
