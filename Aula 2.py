60b5df25bb3d93e8e17b98d8030cfcc23cb3d335
guandrade09 <gustavoandradsousa00@gmail.com>
2024-03-06 21:02:51 -0300

    Primeira aula em Python!

--- Aula 2.py
+++ Aula 2.py
@@ -8,18 +8,51 @@ print("Gustavo Andrade de Sousa")
 
 #Exercício 2
 
-a = 5
-b = 3
+num_1 = 5
+num_2 = 3
+print(num_1)
+print(num_2)
+print(type(num_1))
+print(type(num_2))
+print("a + b")
+print(num_1+num_2)
 
-print(2*a*3*b)
+print("2a * 3b")
+print(2*num_1*3*num_2)
 
 #Exercício 3
 
-c = int(input("Digite o primeiro número: "))
-d = int(input("Digite o segundo número: "))
-e = int(input("Digite o terceiro número: "))
+num_3 = int(input("Digite o primeiro número: "))
+num_4 = int(input("Digite o segundo número: "))
+num_5 = int(input("Digite o terceiro número: "))
 
-soma = c + d + e
+soma = num_3 + num_4 + num_5
 
-print("Resultado:")
-print(soma)
+print("Resultado:", soma)
+
+
+pi = 3.14159
+print(f"número: {pi:.2f}")
+
+pi = 3.14159
+print('número: {:.3f}'.format(pi))
+
+pi = 3.14159
+print("número: %.2f." %pi)
+
+sqr = float
+print("Raiz de 2 = ", float(round(2**(1/2), 4)))
+
+d = True
+print(d)
+print(type(d))
+
+nota = float(input("Coloque sua média: "))
+freq = float(input("Coloque sua frequência: "))
+media = 6
+
+if nota >= 6 and freq >= 75:
+    print("Aprovado! :)")
+else:
+    print("Reprovado. :(")
+
